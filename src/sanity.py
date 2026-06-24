eb0a6d619b1e127404440bf31eda4a5976ddb931
Sneha Malhotra <90023114+snehamalhotra@users.noreply.github.com>
2026-06-25 06:36:20 +1200

    Create sanity.py

---
+++ src/sanity.py
@@ -0,0 +1,6 @@
+import numpy, pandas, scipy
+print("numpy", numpy.__version__)
+print("pandas", pandas.__version__)
+print("scipy", scipy.__version__)
+import numpy as np
+a = np.arange(6).reshape(2,3); print(a, a.dtype, a.shape)
